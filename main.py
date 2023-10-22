f3cc2505de7c916ac083d920f962e15d78531737
mahiv87 <maherreraiv1987@gmail.com>
2023-10-21 21:27:07 -0600

    debugged. not printing last 0

--- main.py
+++ main.py
@@ -1,21 +1,43 @@
+# Welcome message
 print("Welcome to the tip calculator")
 
+# Total cost of the bill
 bill_total = input("What was the total bill?")
 
+# Choose a percentage
 tip_percentage = input("What percentage tip would you like to give? 10, 12, or 15?")
 
+# Input that asks user how the bill will be divided
 bill_split = input("How many people will split the bill?")
 
+# Converting the bill total from string to float
 bill_to_int = float(bill_total)
 
+# Converting percentage from string to int
 tp_to_int = int(tip_percentage)
 
+# Convert percentage to decimal
 percentage = tp_to_int / 100
 
+# Convert divider from string to int
 split_to_int = int(bill_split)
 
-tip = ((bill_to_int * percentage) + bill_to_int) / split_to_int
+# Calculate the tip
+tip = bill_to_int * percentage
 
-currency_converter = f"${round(tip, 2)}"
+# Calculate total bill
+total_bill = bill_to_int + tip
 
+# Calculate how much each person should pay
+bill_per_person = total_bill / split_to_int
+
+# Round the bill per person to two decimal places
+final_amount = round(bill_per_person, 2)
+# Formats the final_amount to use .00
+final_amount = "{:.2f}".format(bill_per_person)
+
+# Convert the tip to currency format using F-String
+currency_converter = f"${final_amount}"
+
+# Print the results
 print(f"Each person should pay: {currency_converter}")
