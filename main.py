7fbdf11f3655814ac27ea5ccb768c685bef03ec7
mahiv87 <maherreraiv1987@gmail.com>
2023-10-21 20:55:46 -0600

    bill split input

--- main.py
+++ main.py
@@ -4,3 +4,4 @@ bill_total = input("What was the total bill?")
 
 tip_percentage = input("What percentage tip would you like to give? 10, 12, or 15?")
 
+bill_split = input("How many people will split the bill?")
