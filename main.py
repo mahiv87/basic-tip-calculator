cfdb0f3f70387a5c31df2f114c201b1f5832d046
mahiv87 <maherreraiv1987@gmail.com>
2023-10-21 20:58:34 -0600

    convert bill total input to int

--- main.py
+++ main.py
@@ -5,3 +5,5 @@ bill_total = input("What was the total bill?")
 tip_percentage = input("What percentage tip would you like to give? 10, 12, or 15?")
 
 bill_split = input("How many people will split the bill?")
+
+bill_to_int = int(bill_total)
